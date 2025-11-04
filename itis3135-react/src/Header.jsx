6652fc7f4ac728f08a0887c554929f1f355a0303
James-Karwowski <james.karwowski@outlook.com>
2025-11-04 15:28:59 -0500

    added footer/header

---
+++ itis3135-react/src/Header.jsx
@@ -0,0 +1,6 @@
+export default function Header(){
+    return <header>
+        <h1>ITIS3135 | James Karwowski-Mulready's | Jumbo King-Mackerel</h1>
+        <nav><a href="../">Home</a> | <a href="./introduction.html">Introduction</a> | <a href="./contract.html">Contract</a> | <a href="./index.html">ITIS3135</a> | <a href="./fccfsjs_outline.html">FCC Fullstack JavaScript Outline</a></nav>
+    </header>;
+}
