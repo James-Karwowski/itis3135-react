8880aae7c3b5413ea1c12ae4d79b90fad9d1a644
James-Karwowski <james.karwowski@outlook.com>
2025-11-09 08:10:33 -0500

    testing

--- Header copy.jsx
+++
@@ -1,6 +0,0 @@
-export default function Header(){
-    return <header>
-        <h1>ITIS3135 | James Karwowski-Mulready's | Jumbo King-Mackerel</h1>
-        <nav><a href="../">Home</a> | <a href="./introduction.html">Introduction</a> | <a href="./contract.html">Contract</a> | <a href="./index.html">ITIS3135</a> | <a href="./fccfsjs_outline.html">FCC Fullstack JavaScript Outline</a></nav>
-    </header>;
-}
