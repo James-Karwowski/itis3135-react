0933b3dd8b63938ec27a3468518bc17be9eaab9e
James-Karwowski <james.karwowski@outlook.com>
2025-11-24 14:39:16 -0500

    fix #2a

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header(){
     return <header>
+        <h1>ITIS3135 | James Karwowski-Mulready's | Jumbo King-Mackerel</h1>
         <nav>
             <Link to="/">Home</Link> | 
             <Link to="/introduction">Introduction</Link> | 
