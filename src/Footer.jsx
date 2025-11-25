26b55cde971e3bb74fe4c725e80e20f52aca94a8
James-Karwowski <james.karwowski@outlook.com>
2025-11-25 12:58:51 -0500

    Fixing issues

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,7 @@
 export default function Footer(){
-    return <footer>
+    return (
+    <footer>
         <p>Designed by James Karwowski-Mulready Designs</p>
-    </footer>;
+    </footer>
+    );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -19,5 +19,5 @@ createRoot(document.getElementById('root')).render(
         </Route>
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
